44156a1b6afea5aabd4da751c316dab21e1d3f03
Sporsho mondal <133359695+rammondal@users.noreply.github.com>
2026-07-17 18:57:14 +0530

    Update pleapher.py

--- pleapher.py
+++ pleapher.py
@@ -1,7 +1,7 @@
 # pip install pyaudio
 
 import pyttsx3 #pip install pyttsx3
-import speech_recognition as sir #pip install speechRecognition
+import speech_recognition as sr #pip install speechRecognition
 import datetime
 import wikipedia #pip install wikipedia
 import webbrowser
